06dd6db31f8f4530cfb2386ebe95c5f40e1df641
Jackson Ming Hu <huming2207@gmail.com>
2022-04-03 10:23:52 +1000

    Update README

--- index.js
+++ index.js
@@ -5,58 +5,86 @@ const { platform, arch } = process
 
 let nativeBinding = null
 let localFileExisted = false
-let isMusl = false
 let loadError = null
+
+function isMusl() {
+  // For Node 10
+  if (!process.report || typeof process.report.getReport !== 'function') {
+    try {
+      return readFileSync('/usr/bin/ldd', 'utf8').includes('musl')
+    } catch (e) {
+      return true
+    }
+  } else {
+    const { glibcVersionRuntime } = process.report.getReport().header
+    return !glibcVersionRuntime
+  }
+}
 
 switch (platform) {
   case 'android':
-    if (arch !== 'arm64') {
-      throw new Error(`Unsupported architecture on Android ${arch}`)
-    }
-    localFileExisted = existsSync(join(__dirname, 'package-template.android-arm64.node'))
-    try {
-      if (localFileExisted) {
-        nativeBinding = require('./package-template.android-arm64.node')
-      } else {
-        nativeBinding = require('@napi-rs/package-template-android-arm64')
-      }
-    } catch (e) {
-      loadError = e
+    switch (arch) {
+      case 'arm64':
+        localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.android-arm64.node'))
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./lycheelink-serv.android-arm64.node')
+          } else {
+            nativeBinding = require('lycheelink-serv-android-arm64')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      case 'arm':
+        localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.android-arm-eabi.node'))
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./lycheelink-serv.android-arm-eabi.node')
+          } else {
+            nativeBinding = require('lycheelink-serv-android-arm-eabi')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
+      default:
+        throw new Error(`Unsupported architecture on Android ${arch}`)
     }
     break
   case 'win32':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'package-template.win32-x64-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.win32-x64-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./package-template.win32-x64-msvc.node')
+            nativeBinding = require('./lycheelink-serv.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-win32-x64-msvc')
+            nativeBinding = require('lycheelink-serv-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'ia32':
-        localFileExisted = existsSync(join(__dirname, 'package-template.win32-ia32-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.win32-ia32-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./package-template.win32-ia32-msvc.node')
+            nativeBinding = require('./lycheelink-serv.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-win32-ia32-msvc')
+            nativeBinding = require('lycheelink-serv-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'package-template.win32-arm64-msvc.node'))
+        localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.win32-arm64-msvc.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./package-template.win32-arm64-msvc.node')
+            nativeBinding = require('./lycheelink-serv.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-win32-arm64-msvc')
+            nativeBinding = require('lycheelink-serv-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -69,24 +97,24 @@ switch (platform) {
   case 'darwin':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'package-template.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./package-template.darwin-x64.node')
+            nativeBinding = require('./lycheelink-serv.darwin-x64.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-darwin-x64')
+            nativeBinding = require('lycheelink-serv-darwin-x64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'package-template.darwin-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.darwin-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./package-template.darwin-arm64.node')
+            nativeBinding = require('./lycheelink-serv.darwin-arm64.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-darwin-arm64')
+            nativeBinding = require('lycheelink-serv-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -100,12 +128,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'package-template.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./package-template.freebsd-x64.node')
+        nativeBinding = require('./lycheelink-serv.freebsd-x64.node')
       } else {
-        nativeBinding = require('@napi-rs/package-template-freebsd-x64')
+        nativeBinding = require('lycheelink-serv-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -114,25 +142,24 @@ switch (platform) {
   case 'linux':
     switch (arch) {
       case 'x64':
-        isMusl = readFileSync('/usr/bin/ldd', 'utf8').includes('musl')
-        if (isMusl) {
-          localFileExisted = existsSync(join(__dirname, 'package-template.linux-x64-musl.node'))
+        if (isMusl()) {
+          localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.linux-x64-musl.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./package-template.linux-x64-musl.node')
+              nativeBinding = require('./lycheelink-serv.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/package-template-linux-x64-musl')
+              nativeBinding = require('lycheelink-serv-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, 'package-template.linux-x64-gnu.node'))
+          localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.linux-x64-gnu.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./package-template.linux-x64-gnu.node')
+              nativeBinding = require('./lycheelink-serv.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/package-template-linux-x64-gnu')
+              nativeBinding = require('lycheelink-serv-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -140,25 +167,24 @@ switch (platform) {
         }
         break
       case 'arm64':
-        isMusl = readFileSync('/usr/bin/ldd', 'utf8').includes('musl')
-        if (isMusl) {
-          localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm64-musl.node'))
+        if (isMusl()) {
+          localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.linux-arm64-musl.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./package-template.linux-arm64-musl.node')
+              nativeBinding = require('./lycheelink-serv.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/package-template-linux-arm64-musl')
+              nativeBinding = require('lycheelink-serv-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm64-gnu.node'))
+          localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.linux-arm64-gnu.node'))
           try {
             if (localFileExisted) {
-              nativeBinding = require('./package-template.linux-arm64-gnu.node')
+              nativeBinding = require('./lycheelink-serv.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/package-template-linux-arm64-gnu')
+              nativeBinding = require('lycheelink-serv-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -166,12 +192,12 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'package-template.linux-arm-gnueabihf.node'))
+        localFileExisted = existsSync(join(__dirname, 'lycheelink-serv.linux-arm-gnueabihf.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./package-template.linux-arm-gnueabihf.node')
+            nativeBinding = require('./lycheelink-serv.linux-arm-gnueabihf.node')
           } else {
-            nativeBinding = require('@napi-rs/package-template-linux-arm-gnueabihf')
+            nativeBinding = require('lycheelink-serv-linux-arm-gnueabihf')
           }
         } catch (e) {
           loadError = e
